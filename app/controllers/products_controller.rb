7ad6552cfc70e9995b797bab70eebe494563249e
shigeno tatsuya <na7na.ten@gmail.com>
2024-05-15 19:33:57 +0900

    wip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,54 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
   
+  def search
+    @products = Product.where(['name LIKE ?', "%#{params[:q]}%"])
+    render "index"
+  end
+  
   def show
     @product = Product.find(params[:id])
+  end
+  
+  def new
+    @product = Product.new
+    @makers = Maker.all
+    @product_types = ProductType.all
+  end
+  
+  def create
+    @makers = Maker.all
+    @product_types = ProductType.all
+    @product = Product.create(product_params)
+  end
+  
+  def edit
+    @product = Product.find(params[:id])
+    @makers = Maker.all
+    @product_types = ProductType.all
+  end
+  
+  # def update
+  #   @product = Product.find(params[:id])
+  #   @product.Product.update(product_params)
+  # end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path, notice: "Product was successfully updated."
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @product = Product.find(params[:id]) 
+    @product.destroy
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:name, :thumbnail, :price, :release_date, :price_range, :photograph_aim, :maker_id, :product_type_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
+  
+  # resources :products
+  
+  resources :products do
+    get "search", on: :collection
+  end
+  
 end
